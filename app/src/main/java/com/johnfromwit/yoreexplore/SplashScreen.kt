8a62cc6ac6369a7d7718d600f075b08f8bef7446
John <7268419+JohnFromWIT@users.noreply.github.com>
2018-03-14 14:40:50 +0000

    Splash Screen:    -Logo
    Issues Resolved:
        -Memory Issue - Resized Image before import

--- app/src/main/java/com/johnfromwit/yoreexplore/SplashScreen.kt
+++ app/src/main/java/com/johnfromwit/yoreexplore/SplashScreen.kt
@@ -2,6 +2,7 @@ package com.johnfromwit.yoreexplore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.johnfromwit.yoreexplore.R
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
